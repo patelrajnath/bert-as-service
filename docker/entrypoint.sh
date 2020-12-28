99922727739e56df72efdd65575ed13ab78bd9dc
Raj Nath Patel <patelrajnath@gmail.com>
2020-12-28 22:29:39 +0000

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bert-serving-start -http_port 8125 -num_worker=$1 -model_dir /model
+bert-serving-start -http_port 8125 -num_worker=$1 -model_dir /model -max_seq_len None -pooling_strategy NONE
